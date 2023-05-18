b828cd454de6b045089ed5733320478091d11827
javmoreno-meli <javier.moreno@mercadolibre.com.co>
2023-05-18 02:28:53 -0500

    termiando solo con 3 enfermedades

--- app.py
+++ app.py
@@ -20,6 +20,18 @@ def realizar_diagnostico():
     if not ('temperatura' in datos and 'dolor_de_cabeza' in datos and 'tos' in datos):
         return jsonify({'mensaje': 'Faltan datos'})
     
+    #si algun dato no es un numero, que no se haga nada
+    if not (isinstance(datos['temperatura'], (int, float)) and isinstance(datos['dolor_de_cabeza'], (int, float)) and isinstance(datos['tos'], (int, float))):
+        return jsonify({'mensaje': 'Los datos deben ser números'})
+    #si algun dato no esta en el rango, que no se haga nada
+    if not (datos['temperatura'] >= 35 and datos['temperatura'] <= 42 and datos['dolor_de_cabeza'] >= 0 and datos['dolor_de_cabeza'] <= 10 and datos['tos'] >= 0 and datos['tos'] <= 10):
+        return jsonify({'mensaje': 'Los datos deben estar en el rango, temperatura: [35, 42], dolor_de_cabeza: [1, 10], tos: [1, 10]'})
+    #Si los datos son iguales o menores a 0, que no se haga nada
+    if not (datos['temperatura'] > 0 and datos['dolor_de_cabeza'] > 0 and datos['tos'] > 0):
+        return jsonify({'mensaje': 'Los datos deben ser mayores a 0'})
+
+
+
 
     #realizar el diagnostico
     resultado = diagnostico(datos)
